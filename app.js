f8ebc146bd0d341faea74aac92a6ec6e85fdba99
Anshu Sarkar <74862392+AnshuSarkarANX@users.noreply.github.com>
2022-10-05 22:47:11 +0530

    Comment updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
   // variable for getting the city  name from the search box
   var cityn = req.body.cityName;
-  // api url
+  //Weather api url
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityn +
